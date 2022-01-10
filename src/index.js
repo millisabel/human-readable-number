43330bef417b51e3d3f2014f3edbaa5c5c045b5c
millisabel <elena.kozlova.job@gmail.com>
2022-01-10 14:17:39 +0200

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let units  = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let tens  = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if(number < 20){
+        return units[number];
+    }
+    if (number >= 20 && number < 100) {
+        return `${tens[Math.floor(number/10)]} ${number%10 ? units[number%10] : ''}`.trim();
+    }
+    return `${units[Math.floor(number/100)]} hundred ${number%100 === 0 ? '' : toReadable(number%100)}`.trim();
 }
+
